e8aa790287745c26d8d4c1592e13d121ff980192
ayaaas <125189175+ayaaas@users.noreply.github.com>
2024-06-10 15:04:58 +0100

    v2

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT, mondoDBURL } = require("./config");
-const subscriptionroute=require("./routes/SubscriptionRoutes")
+const supplierIdentification = require("./Routes/authRoutes");
 
 const mongoose = require("mongoose");
 
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.use("/",subscriptionroute);
+// app.use("/",subscriptionroute);
 
 mongoose
   .connect(mondoDBURL)
