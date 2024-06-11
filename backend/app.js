712ef82c386ec3ddc1a4c20c200b7ce8e157196e
Youssef <youssefzahir.etu@gmail.com>
2024-06-11 13:43:51 +0100

    changes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT, mondoDBURL } = require("./config");
+const subscriptionroute=require("./routes/SubscriptionRoutes")
 
 const mongoose = require("mongoose");
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+app.use("/",subscriptionroute);
 const contractroute=require("./routes/ContractRoutes")
 app.use("/contract",contractroute);
 const negotiationroute=require("./routes/NegotiationRoutes")
@@ -17,7 +20,6 @@ const offerroute=require("./routes/OfferRoutes")
 app.use("/offer",offerroute);
 const pricingmodelroute=require("./routes/PricingModelRoutes")
 app.use("/pricingmodel",pricingmodelroute);
-
 
 mongoose
   .connect(mondoDBURL)
