5a636d57f03c6284237b7918cf4bfc2d88e06e42
ayaaas <125189175+ayaaas@users.noreply.github.com>
2024-06-20 22:07:15 +0100

    V4

--- backend/models/Negociation.models.js
+++ backend/models/Negociation.models.js
@@ -1,12 +1,43 @@
 const mongoose = require('mongoose');
 
 const negotiationSchema = new mongoose.Schema({
-  negotiationId: { type: String, required: true },
+  offerId: {type: mongoose.Schema.Types.ObjectId, ref: 'Offer', required: true },
   buyerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Buyer', required: true },
   supplierId: { type: mongoose.Schema.Types.ObjectId, ref: 'Supplier', required: true },
   order: { type: mongoose.Schema.Types.ObjectId, ref: 'BulkOrder', required: true },
   negotiationStatus: { type: String, enum: ['IN_PROGRESS', 'COMPLETED', 'REJECTED'], default: 'IN_PROGRESS' },
-  messages: [{ type: String }],
+  proposedChanges: {
+    discountPercentage: {
+      type: Number,
+      required: true,
+    },
+    paymentTerms: {
+      type: String,
+      required: true,
+    },
+  },
+  messages: [
+    {
+      sender: { type: mongoose.Schema.Types.ObjectId, required: true },
+      senderType: { type: String, enum: ['Buyer', 'Supplier'], required: true },
+      content: String,
+      timestamp: Date
+    }
+  ],
+  agreementSummary: {
+    discountPercentage: {
+      type: Number,
+      required: true,
+    },
+    orderQuantity: {
+      type: Number,
+      required: true,
+    },
+    paymentTerms: {
+      type: String,
+      required: true,
+    },
+  },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
 });
@@ -14,3 +45,4 @@ const negotiationSchema = new mongoose.Schema({
 const Negotiation = mongoose.model('Negotiation', negotiationSchema);
 
 module.exports = Negotiation ;
+
