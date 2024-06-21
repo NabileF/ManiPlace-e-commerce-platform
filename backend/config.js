21d930366d51b93ef88440616df685d7736fa708
Youssef <youssefzahir.etu@gmail.com>
2024-06-21 10:09:31 +0100

    new changes

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,9 @@
 const PORT = 5400;
 
+const jwtSecret = 'Jwtsecret';
+
+
 const mondoDBURL =
   "mongodb+srv://root:magniplace1234@b2bplatform-mern.tgspbee.mongodb.net/magniplaceDB?retryWrites=true&w=majority&appName=B2Bplatform-MERN";
 
-module.exports = { PORT, mondoDBURL };
+module.exports = { PORT, mondoDBURL, jwtSecret };
