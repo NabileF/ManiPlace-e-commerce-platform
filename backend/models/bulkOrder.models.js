2ff9e57803c2ae20ac43cc5c71e6120c4e1ca9a2
na-ssima <farnass2002@gmail.com>
2024-06-22 19:12:43 +0100

    Add orderModel

--- backend/models/bulkOrder.models.js
+++ backend/models/bulkOrder.models.js
@@ -2,25 +2,21 @@ const mongoose = require('mongoose');
 
 const BulkOrderSchema = new mongoose.Schema({
   supplier: { type: mongoose.Schema.Types.ObjectId, ref: 'Supplier', required: true },
-  orderDetails: [{ productName: String, quantity: Number }],
   buyer: { type: mongoose.Schema.Types.ObjectId, ref: 'Buyer', required: true },
+
+  products: [{
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    quantity: { type: Number, required: true }
+  }],
+
   requestedDeliveryDate: { type: Date, required: true },
   status: { type: String, default: 'Pending' },
   cancellationReason: { type: String, required: false },
-  pricing: { type: Number, required: false },
   deliveryTerms: { type: String, required: false },
-  workflowStages: [{
-    stage: { type: String, required: true },
-    completed: { type: Boolean, default: false },
-    updatedAt: { type: Date, default: Date.now },
-    updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Supplier' }
-  }],
-  negotiationHistory: [{
-    date: { type: Date, default: Date.now },
-    changes: { type: Object, required: true },
-    updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Supplier', required: true }
-  }],
-  rejectionReason: { type: String, required: false }
+
+  rejectionReason: { type: String, required: false },
+
+
 
 });
 
