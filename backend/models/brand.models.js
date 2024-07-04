630504da4d7652d965b63eec170f2ab39cd40631
na-ssima <farnass2002@gmail.com>
2024-07-04 22:32:43 +0100

    add brand schema

---
+++ backend/models/brand.models.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BrandSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const Brand = mongoose.model("Brand", BrandSchema, "brand");
+
+module.exports = Brand;
