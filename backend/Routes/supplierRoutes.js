af5d4c9e86481decfee23411f4a086376dcf6dd9
na-ssima <farnass2002@gmail.com>
2024-07-08 14:40:09 +0100

    add supplier route-salma

--- backend/Routes/supplierRoutes.js
+++ backend/Routes/supplierRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { updateSupplierProfile } = require('../controllers/supplierController');
+const { updateSupplierProfile, updateProductInventory } = require('../controllers/supplierController');
 const { protect } = require('../middlewares/authMiddleware');
 
 router.put('/profile', protect, updateSupplierProfile);
+router.put('/inventory', protect, updateProductInventory); // Nouvelle route pour la mise à jour de l'inventaire
+
 
 module.exports = router;
