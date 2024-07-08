a868bd55c4548f59beecedfe9332bfb2fdaab0e0
na-ssima <farnass2002@gmail.com>
2024-07-08 14:24:48 +0100

    add order routes

--- backend/Routes/bulkOrderRoutes.js
+++ backend/Routes/bulkOrderRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getBulkOrders, createBulkOrder, batchProcessOrders, updateOrderStatus, cancelOrder, negotiateOrder, updateWorkflowStage, rejectOrder} = require('../controllers/bulkOrderController');
+const { getBulkOrders, createBulkOrder, batchProcessOrders, updateOrderStatus, cancelOrder, rejectOrder, confirmOrder} = require('../controllers/bulkOrderController');
 const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
@@ -10,6 +10,8 @@ router.post('/batch-process' ,protect, batchProcessOrders);
 router.put('/:orderId/status' ,protect, updateOrderStatus);
 router.put('/:orderId/cancel' ,protect, cancelOrder);
 router.put('/:orderId/reject', protect, rejectOrder);
+router.put('/:orderId/confirm', protect, confirmOrder); // New route for confirming order
+
 
 
 
